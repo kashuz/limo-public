363ccae69b80677a7ccee7d9f9e4d000f96407f4
Olim Saidov <olim.mail@gmail.com>
2017-10-13 01:41:50 +0500

    Configure shipit deployment tool

---
+++ shipitfile.js
@@ -0,0 +1,44 @@
+const utils = require('shipit-utils');
+
+module.exports = function (shipit) {
+  require('shipit-deploy')(shipit);
+  require('shipit-shared')(shipit);
+
+  shipit.initConfig({
+    default: {
+      key: '~/.ssh/id_rsa',
+      repositoryUrl: 'git@lamuz:lamuz/limo.git',
+      deployTo: '/opt/limo',
+      workspace: '/tmp/shipit',
+      ignores: ['.git', 'node_modules'],
+      keepReleases: 5,
+      shallowClone: true,
+
+      shared: {
+        overwrite: true,
+        files: ['.env', 'pm2.config.js'],
+      }
+    },
+    production: {
+      servers: 'root@limo.uz'
+    }
+  });
+
+  utils.registerTask(shipit, 'setup', async function() {
+    await shipit.remote('yarn install', {cwd: shipit.releasePath});
+    await shipit.remote('yarn db migrate', {cwd: shipit.releasePath});
+    await shipit.remote('yarn build', {cwd: shipit.releasePath});
+  });
+
+  utils.registerTask(shipit, 'start', async function() {
+    await shipit.remote('pm2 startOrRestart pm2.config.js', {cwd: shipit.releasePath});
+  });
+
+  shipit.on('updated', async function () {
+    utils.runTask(shipit, 'setup');
+  });
+
+  shipit.on('published', function () {
+    utils.runTask(shipit, 'start');
+  });
+};
