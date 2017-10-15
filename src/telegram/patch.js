24d1c59063ce2da76802225dee021107c6474531
Olim Saidov <olim.mail@gmail.com>
2017-10-15 13:04:02 +0500

    Globally ignore invalid message errors on delete-message & answer-callback

--- src/telegram/patch.js
+++ src/telegram/patch.js
@@ -1,4 +1,6 @@
 import Context from 'telegraf/lib/core/context';
+import Telegram from 'telegraf/lib/telegram';
+import ignore from '../util/ignore';
 
 Object.defineProperty(Context.prototype, 'from', {
   get: function() {
@@ -13,3 +15,15 @@ Object.defineProperty(Context.prototype, 'from', {
       (this.preCheckoutQuery && this.preCheckoutQuery.from)
   }
 });
+
+const deleteMessage = Telegram.prototype.deleteMessage;
+Telegram.prototype.deleteMessage = function(...args) {
+  return deleteMessage.call(this, ...args)
+    .catch(ignore(/message to delete not found/i));
+};
+
+const answerCallbackQuery = Telegram.prototype.answerCallbackQuery;
+Telegram.prototype.answerCallbackQuery = function(...args) {
+  return answerCallbackQuery.call(this, ...args)
+    .catch(ignore(/query_id_invalid/i));
+};
