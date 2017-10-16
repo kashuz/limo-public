289d3cdf288f389525dcf7f49816e8985aec8b3d
Olim Saidov <olim.mail@gmail.com>
2017-10-16 23:43:10 +0500

    Allow order location within uzbekistan

--- src/util/geo.js
+++ src/util/geo.js
@@ -1,27 +1,35 @@
 import b from 'bluebird';
 import create from 'node-geocoder';
 import r from 'ramda';
+import rs from 'ramdasauce';
 
 const coder = create({
   provider: 'google',
   apiKey: process.env.GOOGLE_MAPS_API_KEY});
 
-const enabled = r.contains(r.__,
-  r.split(',', process.env.ENABLED_CITIES));
+const log = error => {
+  console.error(
+    (e.stack || e.toString())
+      .replace(/^/gm, '  '));
+
+  throw error;
+};
 
 export default function geo(location) {
   return coder
     .reverse({lat: location.latitude, lon: location.longitude})
+    .then(rs.log)
+    .catch(log)
     .then(r.head)
-    .then(r.merge(r.__, location))
-    .then(result => enabled(result.city)
-      ? result
-      : b.reject('Пока заказы принимаются только в Ташкенте'));
+    .then(result => result.countryCode == 'UZ'
+      ? r.merge(result, location)
+      : b.reject('Заказы принимаются только по Узбекистану'));
 }
 
 export function format(location) {
   return r.join(', ',
     r.filter(r.identity, [
+      location.city,
       location.extra && location.extra.neighborhood,
       location.streetName,
       location.streetNumber]));
