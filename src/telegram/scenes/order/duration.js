67bd1a9117e983dfdb1dd229de6e4802679c3ee4
Olim Saidov <olim.mail@gmail.com>
2017-10-16 23:46:00 +0500

    Use persistent messages in duration scene

--- src/telegram/scenes/order/duration.js
+++ src/telegram/scenes/order/duration.js
@@ -1,34 +1,30 @@
 import b from 'bluebird';
 import {Scene} from 'telegraf-flow';
-import db from '../../../db';
-import duration from '../../keyboards/duration';
+import extra from '../../keyboards/duration';
 import update from '../../../sql/update-order';
 
 const scene = new Scene('order.duration');
+const key = 'order.duration.message';
 
-const category = ctx => db('category')
-  .where('id', ctx.flow.state.order.category_id)
-  .first();
-
-const show = ctx => category(ctx)
-  .then(category => ctx.reply('Выберите длительность поездки',
-      duration(ctx.flow.state.order.category, ctx.flow.state.start)));
-
-scene.enter(show);
+scene.enter(ctx =>
+  ctx.persistent.sendMessage(key,'Выберите длительность поездки',
+    extra(ctx.flow.state.order.category, ctx.flow.state.start)));
 
 scene.action(/duration\.(\d+)/, ctx =>
-  update(ctx.flow.state.order.id,
-    {time: ctx.flow.state.start, duration: ctx.match[1]})
+  update(ctx.flow.state.order.id, {time: ctx.flow.state.start, duration: ctx.match[1]})
     .then(order => b.all([
       ctx.answerCallbackQuery('Длительность поездки выбрана'),
-      ctx.deleteMessage(),
+      ctx.persistent.deleteMessage(key),
       ctx.flow.enter('order.create', {order})])));
 
 scene.action('cancel', ctx => b.all([
-  ctx.deleteMessage(),
+  ctx.persistent.deleteMessage(key),
   ctx.flow.enter('order.create', {order: ctx.flow.state.order})]));
 
 scene.use((ctx, next) =>
-  show(ctx).then(() => next()));
+  ctx.persistent.deleteMessage(key)
+    .then(() => ctx.persistent.sendMessage(key, 'Выберите длительность поездки',
+      extra(ctx.flow.state.order.category, ctx.flow.state.start)))
+    .then(() => next()));
 
 export default scene;
