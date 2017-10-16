b18f16605bd7cadbe0b7c4d1b5160d4a4d23aed1
Olim Saidov <olim.mail@gmail.com>
2017-10-16 11:35:15 +0500

    Change create order keyboard

--- src/telegram/keyboards/order-create.js
+++ src/telegram/keyboards/order-create.js
@@ -9,14 +9,14 @@ export default function(order) {
     parse_mode: 'html',
     reply_markup: {
       inline_keyboard: compact([
-        [{text: '📍 Адрес подачи', callback_data: 'location'}],
-        [{text: '🚗 Машина', callback_data: 'car'}],
+        [{text: '📍 Адрес подачи', callback_data: 'location'},
+         {text: '🚗 Машина', callback_data: 'car'}],
         [{text: '🗓 Дата', callback_data: 'date'},
           {text: '⏰ Время', callback_data: 'start-time'}],
         [{text: `${order.payment === 'payme' ? '◼️' : '◻️'} Payme`, callback_data: 'payment.payme'},
           {text: `${order.payment === 'cash' ? '◼️' : '◻️'} Наличные`, callback_data: 'payment.cash'}],
-        [{text: '📝 Комментарий', callback_data: 'note'}],
-        [{text: '☎️ Контактный номер', callback_data: 'phone-number'}],
+        [{text: '📝 Комментарий', callback_data: 'note'},
+         {text: '☎️ Номер', callback_data: 'phone-number'}],
         ready(order) &&
           [{text: '✅ Отправить заказ', callback_data: 'submit'}],
         [{text: '❌ Отмена', callback_data: 'cancel'}]])}};
