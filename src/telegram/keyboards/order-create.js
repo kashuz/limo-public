c0c9a700f79e74763a68bbccacf726268168c823
Olim Saidov <olim.mail@gmail.com>
2017-10-23 11:43:40 +0500

    Add check marks into order-create keyboard

--- src/telegram/keyboards/order-create.js
+++ src/telegram/keyboards/order-create.js
@@ -5,14 +5,28 @@ export default function(order) {
     parse_mode: 'html',
     reply_markup: {
       inline_keyboard: compact([
-        [{text: '📍 Адрес подачи', callback_data: 'location'},
-         {text: '🚗 Машина', callback_data: 'car'}],
-        [{text: '🗓 Дата', callback_data: 'date'},
-         {text: '⏰ Время', callback_data: 'time'}],
-        [{text: `${order.payment === 'payme' ? '◼️' : '◻️'} Payme`, callback_data: 'payment.payme'},
-         {text: `${order.payment === 'cash' ? '◼️' : '◻️'} Наличные`, callback_data: 'payment.cash'}],
-        [{text: '📝 Комментарий', callback_data: 'note'},
-         {text: '☎️ Номер', callback_data: 'phone-number'}],
-        [{text: '✅ Отправить заказ', callback_data: 'submit'}],
-        [{text: '❌ Отмена', callback_data: 'cancel'}]])}};
+        [{text: order.location ? '📍 Адрес подачи ✓' : '📍 Адрес подачи',
+          callback_data: 'location'},
+         {text: order.category_id ? '🚗 Машина ✓' : '🚗 Машина',
+          callback_data: 'car'}],
+
+        [{text: order.date ? '🗓 Дата ✓' : '🗓 Дата',
+          callback_data: 'date'},
+         {text: order.time ? '⏰ Время ✓' : '⏰ Время',
+          callback_data: 'time'}],
+
+        [{text: `${order.payment === 'payme' ? '◼️' : '◻️'} Payme ${order.payment === 'payme' ? '✓' : ''}`,
+          callback_data: 'payment.payme'},
+         {text: `${order.payment === 'cash' ? '◼️' : '◻️'} Наличные ${order.payment === 'cash' ? '✓' : ''}`,
+          callback_data: 'payment.cash'}],
+
+        [{text: order.note ? '📝 Комментарий ✓' : '📝 Комментарий',
+          callback_data: 'note'},
+         {text: (order.phone_number || order.user.phone_number) ? '☎️ Номер ✓' : '☎️ Номер',
+          callback_data: 'phone-number'}],
+
+        [{text: '✅ Отправить заказ',
+          callback_data: 'submit'}],
+        [{text: '❌ Отмена',
+          callback_data: 'cancel'}]])}};
 }
